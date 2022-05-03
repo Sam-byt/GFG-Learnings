36152fb2ce32fa6408f4d8cf954c85117166019e
Shambo Roy <67813512+Sam-byt@users.noreply.github.com>
2022-05-03 10:45:44 +0530

    Nth catalan number
    Bottom Up DP
    Recursion Formula

---
+++ Number Theory/Basics/5i.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <boost/multiprecision/cpp_int.hpp>
+using boost::multiprecision::cpp_int;  // https://www.geeksforgeeks.org/factorial-large-number-using-boost-multiprecision-library/
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+    
+    public:
+    //Function to find the nth catalan number.
+    cpp_int findCatalan(int n) 
+    {
+        cpp_int t[102];
+        cpp_int value,ans;
+        t[0] = t[1] = 1;
+        for(int i=2;i<=n;i++){
+            ans = 0;
+            for(int j=1;j<=n;j++){
+                value = i-j<0?0:t[i-j]; 
+                ans += t[j-1]*value;
+            }
+            t[i] = ans;
+        }
+        
+        return t[n];
+    }
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+    //taking count of testcases
+	int t;
+	cin>>t;
+	while(t--) {
+	    
+	    //taking nth number
+	    int n;
+	    cin>>n;
+	    Solution obj;
+	    //calling function findCatalan function
+	    cout<< obj.findCatalan(n) <<"\n";    
+	}
+	return 0;
+}  // } Driver Code Ends
